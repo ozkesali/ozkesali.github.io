b23d9bc82dcb31c4aeb682e99d74c72c628f1231
ozkesali <90681413+ozkesali@users.noreply.github.com>
2023-04-08 10:24:42 +0200

    Update searchmid.py

--- searchmid.py
+++ searchmid.py
@@ -1,9 +1,22 @@
 import pandas as pd
 
-df = pd.read_excel('/assets/MidtermScores.xlsx', index_col='Student_number')
+df = pd.read_excel('assets/MidtermScores.xlsx', index_col='Student_number')
 
 def get_value(Student_number):
     try:
         return df.loc[Student_number]['Total']
     except KeyError:
         return None
+
+if __name__ == '__main__':
+    import cgi
+
+    form = cgi.FieldStorage()
+    Student_number = form.getvalue('Student_number')
+
+    value = get_value(Student_number)
+
+    if value is None:
+        print('Error: ID number not found')
+    else:
+        print(value)
